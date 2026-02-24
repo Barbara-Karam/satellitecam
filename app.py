99be21b3dc13357d94a977e5dee7b711d927170b
Barbara Karam <brbarakaram1@gmail.com>
2026-02-25 00:47:32 +0200

    Change default port from 5000 to 10000

--- app.py
+++ app.py
@@ -92,5 +92,5 @@ def gallery():
     return render_template_string(html)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
